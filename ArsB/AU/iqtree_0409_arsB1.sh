51e305e83a2fcf09cc713ca44491b1fa360fc4f1
GiuliaRibeiro <41350697+GiuliaRibeiro@users.noreply.github.com>
2021-11-26 16:25:10 +0100

    Delete ArsB/AU directory

--- ArsB/AU/iqtree_0409_arsB1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#$ -S /bin/sh
-
-
-/home/fernando/GMR/iqtree-1.6.12-Linux/bin/iqtree -s /home/fernando/GMR/ArsB_homologs_RiddedSeqs_v15_MAFFT_80trim.fasta -m LG+F+I+G4 -g /home/fernando/GMR/edited_tree_eukmono.constr1 -pre /home/fernando/GMR/ArsB.constr1 -nt 10
